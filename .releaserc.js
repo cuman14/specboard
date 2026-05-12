dbe260aae88f93dfdf88f902463c9610da4855df
Oswald Alessandro <cuman74@gmail.com>
2026-05-12 19:35:36 +0200

    revert(ci): remove curl trigger, use native tag push trigger

--- .releaserc.js
+++ .releaserc.js
@@ -12,13 +12,5 @@ export default {
     ],
     "@semantic-release/git",
     "@semantic-release/github",
-    // Trigger release workflow after creating tag
-    [
-      "@semantic-release/exec",
-      {
-        successCmd:
-          'curl -X POST -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/cuman14/specboard/actions/workflows/release.yml/dispatches -d \'{"ref":"main","inputs":{"tag":"${nextRelease.version}"}}\'',
-      },
-    ],
   ],
 };
